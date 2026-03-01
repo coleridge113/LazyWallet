96a4a3b8fc7f5b50dd1e6e46616b155a02c988a2
Jose Luna <josemanuelmluna@gmail.com>
2026-03-01 11:25:27 +0800

    feat: restricted height of category dropdown menu

--- app/src/main/java/com/luna/budgetapp/presentation/screen/expensepreset/components/ExpensePresetDialog.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/expensepreset/components/ExpensePresetDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.input.rememberTextFieldState
@@ -118,7 +119,8 @@ fun ExpensePresetDialog(
 
                     ExposedDropdownMenu(
                         expanded = expanded,
-                        onDismissRequest = { expanded = false}
+                        onDismissRequest = { expanded = false},
+                        modifier = Modifier.heightIn(max = 226.dp)
                     ) {
                         options.forEach { option ->
                             DropdownMenuItem(
