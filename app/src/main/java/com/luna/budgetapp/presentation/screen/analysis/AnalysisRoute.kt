51a970d8eb713fba287cb0b79258514d9715d92c
Jose Luna <josemanuelmluna@gmail.com>
2026-03-09 10:58:22 +0800

    feat: initialized setup for Analysis Route

--- app/src/main/java/com/luna/budgetapp/presentation/screen/analysis/AnalysisRoute.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/analysis/AnalysisRoute.kt
@@ -1,12 +1,62 @@
 package com.luna.budgetapp.presentation.screen.analysis
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.getValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AnalysisRoute(
     navController: NavController,
     viewModel: AnalysisViewModel
+) {
+   
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier,
+                title = {},
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBackIosNew,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+
+        MainContent(
+            modifier = Modifier.padding(innerPadding),
+            uiState = uiState,
+            onEvent = viewModel::onEvent,
+        )
+    }
+}
+
+@Composable
+fun MainContent(
+    modifier: Modifier,
+    uiState: UiState,
+    onEvent: (Event) -> Unit
 ) {
 
 }
--- app/src/main/java/com/luna/budgetapp/presentation/screen/analysis/AnalysisViewModel.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/analysis/AnalysisViewModel.kt
@@ -4,9 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.luna.budgetapp.domain.usecase.UseCases
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class AnalysisViewModel (
     private val useCases: UseCases
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun onEvent(event: Event) {}
 }
