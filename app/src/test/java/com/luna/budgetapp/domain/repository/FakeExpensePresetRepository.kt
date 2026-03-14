e5b4d0155df7b8a1f3c71e588b088a0b50a116cb
Jose Luna <josemanuelmluna@gmail.com>
2026-03-14 09:58:09 +0800

    style: renamed flow to presetFlow

--- app/src/test/java/com/luna/budgetapp/domain/repository/FakeExpensePresetRepository.kt
+++ app/src/test/java/com/luna/budgetapp/domain/repository/FakeExpensePresetRepository.kt
@@ -1,28 +1,28 @@
 package com.luna.budgetapp.domain.repository
 
+import com.luna.budgetapp.domain.model.Expense
 import com.luna.budgetapp.domain.model.ExpensePreset
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
 
 class FakeExpensePresetRepository : ExpensePresetRepository {
-    private val flow = MutableStateFlow<List<ExpensePreset>>(emptyList())
+    private val presetFlow = MutableStateFlow<List<ExpensePreset>>(emptyList())
 
-    override fun getAllExpensePresets(): Flow<List<ExpensePreset>> = flow
+    override fun getAllExpensePresets(): Flow<List<ExpensePreset>> = presetFlow
 
     override suspend fun addExpensePresets(expensePresets: List<ExpensePreset>) {
         TODO("Not yet implemented")
     }
 
     override suspend fun addExpensePreset(expensePreset: ExpensePreset) {
-        flow.update {
+        presetFlow.update {
            it + expensePreset
         }
     }
 
     override suspend fun deleteExpensePreset(expensePresetId: Long) {
-        flow.update { presetList ->
+        presetFlow.update { presetList ->
             presetList.filter { it.id != expensePresetId }
         }
     }
