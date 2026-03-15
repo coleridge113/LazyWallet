2583ddbd58b7d5fd74884783c851a04e86b77c9c
Jose Luna <josemanuelmluna@gmail.com>
2026-03-15 13:04:25 +0800

    test: implemented methods for fake category repo

--- app/src/test/java/com/luna/budgetapp/domain/repository/FakeCategoryRepository.kt
+++ app/src/test/java/com/luna/budgetapp/domain/repository/FakeCategoryRepository.kt
@@ -1,30 +1,60 @@
 package com.luna.budgetapp.domain.repository
 
+import com.luna.budgetapp.domain.model.Category
 import com.luna.budgetapp.domain.model.CategoryFilter
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
 
 class FakeCategoryRepository : CategoryRepository {
+
+    private val profilesFlow =
+        MutableStateFlow<Map<String, List<CategoryFilter>>>(emptyMap())
+
     override fun getProfiles(): Flow<List<String>> {
-        TODO("Not yet implemented")
+        return profilesFlow.map { it.keys.toList() }
     }
 
     override fun getProfile(profileName: String): Flow<List<CategoryFilter>> {
-        TODO("Not yet implemented")
+        return profilesFlow.map { profiles ->
+            profiles[profileName] ?: emptyList()
+        }
     }
 
     override suspend fun saveProfile(items: List<CategoryFilter>) {
-        TODO("Not yet implemented")
+        if (items.isEmpty()) return
+
+        val profileName = items.first().profileName
+
+        profilesFlow.update { profiles ->
+            profiles + (profileName to items)
+        }
     }
 
     override suspend fun deleteProfile(profileName: String) {
-        TODO("Not yet implemented")
+        profilesFlow.update { profiles ->
+            profiles - profileName
+        }
     }
 
     override suspend fun hasAny(): Boolean {
-        TODO("Not yet implemented")
+        return profilesFlow.value.isNotEmpty()
     }
 
     override suspend fun initializeIfNeeded() {
-        TODO("Not yet implemented")
+        if (profilesFlow.value.isNotEmpty()) return
+
+        val profileName = "All"
+
+        val items = Category.entries.map { category ->
+            CategoryFilter(
+                profileName = profileName,
+                category = category,
+                isActive = true
+            )
+        }
+
+        profilesFlow.value = mapOf(profileName to items)
     }
 }
