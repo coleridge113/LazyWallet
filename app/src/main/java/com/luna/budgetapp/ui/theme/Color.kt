06a593a2041fec30646548ac8cf6bb9a48c076d1
Jose Luna <josemanuelmluna@gmail.com>
2026-05-24 13:57:49 +0800

    feat: added gruvbox color theme

--- app/src/main/java/com/luna/budgetapp/ui/theme/Color.kt
+++ app/src/main/java/com/luna/budgetapp/ui/theme/Color.kt
@@ -26,3 +26,30 @@ val GroceryChartColor = Color(0xFF4CAF50)     // Fresh Green
 val FitnessChartColor = Color(0xFFFF8F00)     // Amber 800
 val PersonalChartColor = Color(0xFF4DD0E1)    // Cyan 300
 val OthersChartColor = Color(0xFF616161)      // Grey
+
+// Gruvbox Material Colors
+val GruvboxBg0 = Color(0xFF282828)
+val GruvboxBg1 = Color(0xFF32302f)
+val GruvboxBg2 = Color(0xFF3c3836)
+val GruvboxFg0 = Color(0xFFdfbf8e)
+val GruvboxFg1 = Color(0xFFd4be98)
+val GruvboxRed = Color(0xFFea6962)
+val GruvboxOrange = Color(0xFFe78a4e)
+val GruvboxYellow = Color(0xFFd8a657)
+val GruvboxGreen = Color(0xFFa9b665)
+val GruvboxAqua = Color(0xFF89b482)
+val GruvboxBlue = Color(0xFF7daea3)
+val GruvboxPurple = Color(0xFFd3869b)
+val GruvboxGray = Color(0xFF928374)
+
+// Gruvbox Material Light
+val GruvboxLightBg0 = Color(0xFFfbf1c7)
+val GruvboxLightBg1 = Color(0xFFf4e8be)
+val GruvboxLightFg0 = Color(0xFF654735)
+val GruvboxLightRed = Color(0xFFc14a4a)
+val GruvboxLightOrange = Color(0xFFe78a4e)
+val GruvboxLightYellow = Color(0xFFb47109)
+val GruvboxLightGreen = Color(0xFF6c782e)
+val GruvboxLightAqua = Color(0xFF4c7a5d)
+val GruvboxLightBlue = Color(0xFF45707a)
+val GruvboxLightPurple = Color(0xFF945e80)
