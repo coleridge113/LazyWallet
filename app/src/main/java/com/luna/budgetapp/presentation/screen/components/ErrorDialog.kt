92797c5c1650dba651c0194cbee148c6eddeb538
Jose Luna <coleridge@Icarus.local>
2026-05-29 17:23:41 +0800

    refactor: made message nullable

--- app/src/main/java/com/luna/budgetapp/presentation/screen/components/ErrorDialog.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/components/ErrorDialog.kt
@@ -8,31 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ErrorOutline
 import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.room.util.TableInfo
 import com.luna.budgetapp.ui.theme.LazyWalletTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ErrorDialog(
-    message: String,
+    message: String? = null,
     onDismiss: () -> Unit
 ) {
     BasicAlertDialog(
@@ -53,7 +44,7 @@ fun ErrorDialog(
                     .fillMaxSize(),
             ) {
                 Text(
-                    text = message,
+                    text = message ?: "Something went wrong",
                     style = MaterialTheme.typography.bodyLarge,
                     color = MaterialTheme.colorScheme.onSurface
                 )
