9b000977d0c2d69fbc7cdb8985616c2fc3d3cee5
Jose Luna <josemanuelmluna@gmail.com>
2026-07-16 22:36:22 +0800

    chore: cleaned up imports

--- app/src/main/java/com/luna/budgetapp/presentation/screen/analysis/AnalysisRoute.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/analysis/AnalysisRoute.kt
@@ -10,16 +10,13 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
