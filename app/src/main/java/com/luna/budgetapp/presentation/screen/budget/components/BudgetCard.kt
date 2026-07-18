66093547ca03e8dbe0456f4c18a945fe434c2928
Jose Luna <josemanuelmluna@gmail.com>
2026-07-18 14:49:50 +0800

    feat: added drop down menu for BudgetCard

--- app/src/main/java/com/luna/budgetapp/presentation/screen/budget/components/BudgetCard.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/budget/components/BudgetCard.kt
@@ -1,24 +1,33 @@
 package com.luna.budgetapp.presentation.screen.budget.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -29,13 +38,12 @@ import androidx.compose.ui.unit.dp
 import com.luna.budgetapp.domain.model.Category
 import com.luna.budgetapp.domain.model.Budget
 import com.luna.budgetapp.domain.model.DateFilter
-import com.luna.budgetapp.domain.model.Expense
 import com.luna.budgetapp.presentation.screen.utils.getIconForCategory
 import com.luna.budgetapp.presentation.screen.utils.singleClick
 import com.luna.budgetapp.presentation.screen.utils.toCurrency
 import com.luna.budgetapp.ui.theme.LazyWalletTheme
 import java.time.LocalDate
-import java.time.LocalDateTime
+import java.util.Locale
 
 @Composable
 fun BudgetCard(
@@ -66,13 +74,28 @@ fun BudgetCard(
                     modifier = Modifier.padding(bottom = 8.dp)
                 )
                 Spacer(modifier = Modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Default.Edit,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(18.dp)
-                        .singleClick { onEdit(budget) }
-                )
+                Box {
+                    var expanded by remember { mutableStateOf(false) }
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(18.dp)
+                            .singleClick { expanded = true }
+                    )
+                    BudgetCardMenu(
+                        modifier = Modifier
+                            .width(90.dp),
+                        expanded = expanded,
+                        onDismiss = { expanded = false },
+                        onClick = { option ->
+                            when (option) {
+                                Options.EDIT -> onEdit(budget)
+                                Options.DELETE -> {}
+                            }
+                        }
+                    )
+                }
             }
 
             // Financials section
@@ -163,6 +186,40 @@ fun BudgetCard(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun BudgetCardMenu(
+    modifier: Modifier = Modifier,
+    expanded: Boolean,
+    onDismiss: () -> Unit,
+    onClick: (Options) -> Unit
+) {
+    val options = Options.entries
+
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = onDismiss,
+        modifier = modifier
+    ) {
+        options.forEach { option ->
+            DropdownMenuItem(
+                text = { Text(option.getDisplayName()) },
+                onClick = {
+                    onClick(option)
+                }
+            )
+        }
+    }
+}
+
+private enum class Options {
+    EDIT,
+    DELETE;
+
+    fun getDisplayName(): String {
+        return name.lowercase(Locale.getDefault()).replaceFirstChar { it.uppercase() }
     }
 }
 
