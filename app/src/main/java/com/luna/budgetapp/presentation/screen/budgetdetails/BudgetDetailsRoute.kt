68a16648e5ca078b065e5536d1f320dc5e4b8914
Jose Luna <josemanuelmluna@gmail.com>
2026-07-22 20:46:40 +0800

    wip: partial implementation of budget details screen

--- app/src/main/java/com/luna/budgetapp/presentation/screen/budgetdetails/BudgetDetailsRoute.kt
+++ app/src/main/java/com/luna/budgetapp/presentation/screen/budgetdetails/BudgetDetailsRoute.kt
@@ -1,20 +1,65 @@
 package com.luna.budgetapp.presentation.screen.budgetdetails
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import com.luna.budgetapp.presentation.screen.components.ExpenseTable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun BudgetDetailsRoute(
-    navController: NavHostController,
+    navController: NavController,
     viewModel: BudgetDetailsViewModel = koinViewModel()
 ) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    when (val state = uiState) {
+        UiState.Loading -> {}
+        is UiState.Error -> {}
+        is UiState.Success -> {
+            Scaffold(
+                topBar = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Text(
+                            text = state.budget.name,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(Modifier.weight(1f))
+                    }
+                }
+            ) { innerPadding ->
+                MainContent(
+                    modifier = Modifier.padding(innerPadding),
+                    state = state
+                )
+            }
+        }
+    }
 }
 
 @Composable
-private fun MainContent() {
+private fun MainContent(
+    modifier: Modifier,
+    state: UiState.Success
+) {
+    val (budget, expenses) = state
 
 }
 
